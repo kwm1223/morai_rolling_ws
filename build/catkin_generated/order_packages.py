47dcfe02ceec99b132a4fa2f3099ac954aeb95ba
kwm1223 <kwm1223@naver.com>
2024-03-19 16:01:10 +0900

    add catkin_make folder tree

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kwm1223/morai_rolling_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/kwm1223/rolling_ws/devel;/opt/ros/noetic'.split(';') if '/home/kwm1223/rolling_ws/devel;/opt/ros/noetic' != '' else []
